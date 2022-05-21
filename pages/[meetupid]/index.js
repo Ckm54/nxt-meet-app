c07083f2dfec2163e254274ee4899ac440b522e7
Collins <mutaic84@gmail.com>
2022-05-21 10:23:17 +0300

    handle navigating to detail page and update page's styling

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+function MeetupDetails() {
+  return <MeetupDetail 
+    image="https://images.unsplash.com/photo-1610374792793-f016b77ca51a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVldGluZyUyMHJvb218ZW58MHx8MHx8&auto=format&fit=crop&w=500"
+    title="First meetup"
+    address="some place 5, city nvgd"
+    description="This is our first meetup"
+  />
+}
+
+export default MeetupDetails
